9fec85ac2672fd9fb2d65e2362d06b5c171ff5f7
kleberson154 <kleberson55@hotmail.com>
2023-12-22 13:04:23 -0300

    Adicionando a funcao calculator para automatizar os calculos

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function callDiv(a, b) {
   total = a / b
   console.log(total)
 }
+
+function callculator(a, b, operator) {
+  a = parseFloat(prompt('Digite o primeiro numero'))
+  operator = prompt('Digite o operador')
+  b = parseFloat(prompt('Digite o segundo numero'))
+
+  switch (operator) {
+    case '+':
+      callAdd(a, b)
+      break
+    case '-':
+      callSub(a, b)
+      break
+    case '*':
+      callMult(a, b)
+      break
+    case '/':
+      callDiv(a, b)
+      break
+  }
+}
