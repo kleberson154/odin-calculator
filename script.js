4685890f59022e6cf6b588ce22817f0872969a3e
kleberson154 <kleberson55@hotmail.com>
2023-12-22 12:49:14 -0300

    criando as operacoes basicas de adicao,subtracao, multiplicacao e divisao

---
+++ script.js
@@ -0,0 +1,19 @@
+function callAdd(a, b) {
+  total = a + b
+  console.log(total)
+}
+
+function callSub(a, b) {
+  total = a - b
+  console.log(total)
+}
+
+function callMult(a, b) {
+  total = a * b
+  console.log(total)
+}
+
+function callDiv(a, b) {
+  total = a / b
+  console.log(total)
+}
